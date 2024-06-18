498b05c86167741f9317188e088f8704315937c0
HollyIzza <133838311+Syafi26@users.noreply.github.com>
2024-06-19 01:31:01 +0800

    Add files via upload

---
+++ EncodingGenerator.py
@@ -0,0 +1,39 @@
+import cv2
+import face_recognition
+import pickle as pkl
+import os
+
+#Importing images
+folderPath = 'C:/UNISTUDIES/FINAL YEAR 1ST SEMESTER/SIDP/Facial Recognition/SIDP Face Recognition/images'
+PathList = os.listdir(folderPath)
+imgList = []
+individual_ID=[]
+
+for path in PathList:
+    imgList.append(cv2.imread(os.path.join(folderPath,path)))
+    individual_ID.append(os.path.splitext(path)[0])
+
+print(individual_ID)
+
+
+#Creating the encodings
+def findEncodings(imagesList):
+
+    encoded_list = []
+    for img in imagesList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encoded_list.append(encode)
+
+    return encoded_list
+
+print("Encoding Started")
+encodeListKnown = findEncodings(imgList)
+encodeListKnown_withID = [encodeListKnown, individual_ID]
+print("Encoding Complete")
+
+file = open("EncodedFile.p", "wb")
+pkl.dump(encodeListKnown_withID,file)
+file.close()
+print("File Saved")
+
