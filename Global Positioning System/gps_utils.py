d515143989b6d82821890ccc2af0e2c450eb8023
Adli Naufal <adlinaufal07@gmail.com>
2024-06-29 16:57:12 +0800

    GPS Coding

--- Global Positioning System/gps_utils.py
+++ Global Positioning System/gps_utils.py
@@ -41,30 +41,26 @@ def GetGPSData(gps):
         if msg_list[GPGSA_dict['msg_id']] == "$GPGSA":
             if msg_list[GPGSA_dict['mode2']] == "1":
                 print("!!!!!!!Failed to obtain GPS coordinates.!!!!!!!\n")
-                utc_time = "N/A" 
-                latitude = "N/A" 
-                longitude = "N/A"
+                return None, None, None
 
             else:
-                print("***GPS coordinates successfully retrieved.\n***")
-        
-                if msg_list[GPGGA_dict['msg_id']] == "$GPGGA":
-                    for key, value in GPGGA_dict.items():
-                        if key == "utc":
-                            utc_str = msg_list[GPGGA_dict[key]]
-                            if utc_str:
-                                h = int(utc_str[0:2])
-                                m = int(utc_str[2:4])
-                                s = float(utc_str[4:])
-                                utc_time = "{}:{}:{}".format(h, m, s)
+                print("***GPS coordinates successfully retrieved.***\n")
 
-                        elif key == "latitude":
-                            latitude = msg_list[GPGGA_dict[key]]
-
-                        elif key == "longitude":
-                                longitude = msg_list[GPGGA_dict[key]]
+        if msg_list[GPGGA_dict['msg_id']] == "$GPGGA":
+            for key, value in GPGGA_dict.items():
+                if key == "utc":
+                    utc_str = msg_list[GPGGA_dict[key]]
+                    if utc_str:
+                        h = int(utc_str[0:2])
+                        m = int(utc_str[2:4])
+                        s = float(utc_str[4:])
+                        utc_time = "{}:{}:{}".format(h, m, s
+                elif key == "latitude":
+                    latitude = msg_list[GPGGA_dict[key]
+                elif key == "longitude":
+                        longitude = msg_list[GPGGA_dict[key]]
+    
+    if utc_time and latitude and longitude:
+        return utc_time, latitude, longitude
             
-            if utc_time and latitude and longitude:
-                break
-
-    return utc_time, latitude, longitude
+    return None, None, None
