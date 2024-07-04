654893478334b655f67f265f49db17af82150e93
Adli Naufal <adlinaufal07@gmail.com>
2024-07-04 18:07:37 +0800

    testing

--- GPS/gps_utils_testing.py
+++ GPS/gps_utils_testing.py
@@ -87,8 +87,11 @@ def IsValidGpsinfo(gps):
 
 def main():
     gps = serial.Serial(uart_port, baudrate=9600, timeout=0.5)
+    while True:
+        IsValidGpsinfo(gps)
+        time.sleep(1)
     
-    IsValidGpsinfo(gps)
-             
+    gps.close()
+                
 if __name__ == "__main__":
     sys.exit(main())
