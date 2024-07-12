82202b172025d7d50a49b501ff623d3c4fb455ca
Adli Naufal <adlinaufal07@gmail.com>
2024-07-12 20:46:00 +0800

    testing

--- GPS/main_testing.py
+++ GPS/main_testing.py
@@ -1,22 +1,15 @@
 import sys
-import time
 import serial
-from gps_utils_testing import GetGPSData
-
-uart_port = "/dev/ttyS0"
+from gps_utils import IsValidGpsinfo, uart_port
 
 def main():
     gps = serial.Serial(uart_port, baudrate=9600, timeout=0.5)
     while True:
-        latitude, longitude = GetGPSData(gps)
-        if latitude and longitude:
+        latitude, longitude = IsValidGpsinfo(gps)
+        if latitude is not None and longitude is not None:
             print("\nExtracted Coordinates:")
-            print("Latitude: {} degrees {} minutes {}".format(latitude[0], latitude[1], latitude[2]))
-            print("Longitude: {} degrees {} minutes {}".format(longitude[0], longitude[1], longitude[2]))
-            break  # Stop execution once the values are retrieved and printed
-        time.sleep(1)
-    
-    gps.close()
+            print("{:.6f}, {:.6f}".format(latitude, longitude))
+            break
 
 if __name__ == "__main__":
     sys.exit(main())
