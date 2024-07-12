1ae1a4cb3cc53a4c6c0bda50fd40b554161ec55a
Adli Naufal <adlinaufal07@gmail.com>
2024-07-12 20:26:38 +0800

    testing

--- GPS/testing.py
+++ GPS/testing.py
@@ -33,7 +33,7 @@ GPGGA_dict = {
 
 uart_port = "/dev/ttyS0"
 
-def GetGPSData(gps):
+def IsValidGpsinfo(gps):
     data = gps.readline()
     msg_str = str(data, encoding="utf-8")
     msg_list = msg_str.split(",")
@@ -45,40 +45,39 @@ def GetGPSData(gps):
         print()
         if msg_list[GPGSA_dict['mode2']] == "1":
             print("!!!!!!Positioning is invalid!!!!!!")
-        else:
-            if msg_list[GPGGA_dict['msg_id']] == "$GPGGA":
-                print()
-                print("*****The GGA info is as follows: *****")
-                for key, value in GPGGA_dict.items():
-                    if key == "latitude":
-                        lat_str = msg_list[GPGGA_dict[key]]
-                        if lat_str != '':
-                            Len = len(lat_str.split(".")[0])
-                            d = int(lat_str[0:Len-2])
-                            m = float(lat_str[Len-2:])
-                            latitude1 = lat_str;
-                            latitude2 = (d, m, msg_list[GPGGA_dict['NorS']])
-                    elif key == "longitude":
-                        lon_str = msg_list[GPGGA_dict[key]]
-                        if lon_str != '':
-                            Len = len(lon_str.split(".")[0])
-                            d = int(lon_str[0:Len-2])
-                            m = float(lon_str[Len-2:])
-                            longitude1 = lon_str
-                            longitude2 = (d, m, msg_list[GPGGA_dict['EorW']])
-                    else:
-                        print(" {}: {}".format(key, msg_list[GPGGA_dict[key]]))
+            return None, None
 
+    if msg_list[GPGGA_dict['msg_id']] == "$GPGGA":
+        print()
+        print("*****The GGA info is as follows: *****")
+        for key, value in GPGGA_dict.items():
+            if key == "latitude":
+                lat_str = msg_list[GPGGA_dict[key]]
+                if lat_str != '':
+                    Len = len(lat_str.split(".")[0])
+                    d = int(lat_str[0:Len-2])
+                    m = float(lat_str[Len-2:])
+                    latitude = lat_str
+                    #latitude = (d, m, msg_list[GPGGA_dict['NorS']])
+            elif key == "longitude":
+                lon_str = msg_list[GPGGA_dict[key]]
+                if lon_str != '':
+                    Len = len(lon_str.split(".")[0])
+                    d = int(lon_str[0:Len-2])
+                    m = float(lon_str[Len-2:])
+                    longitude = lon_str
+                    #longitude = (d, m, msg_list[GPGGA_dict['EorW']])
+    
     return latitude, longitude
 
 def main():
     gps = serial.Serial(uart_port, baudrate=9600, timeout=0.5)
     while True:
-        latitude, longitude = GetGPSData(gps)
+        latitude, longitude = IsValidGpsinfo(gps)
         if latitude and longitude:
             print("\nExtracted Coordinates:")
-            print("Latitude: {}"(latitude))
-            print("Longitude: {}"(longitude))
+            print("Latitude: {} degrees {} minutes {}".format(latitude[0], latitude[1], latitude[2]))
+            print("Longitude: {} degrees {} minutes {}".format(longitude[0], longitude[1], longitude[2]))
             break
     
 if __name__ == "__main__":
