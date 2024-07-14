d9c4f38a90e44ebcfb4ae67a5fc594876cb23e31
Adli Naufal <adlinaufal07@gmail.com>
2024-07-15 00:22:25 +0800

    Fixed error inside function CoordinatestoLocation

--- Integration/main_gps.py
+++ Integration/main_gps.py
@@ -8,8 +8,8 @@ def main():
     gps = serial.Serial(uart_port, baudrate=9600, timeout=0.5)
     while True:
         latitude, longitude = GetGPSData(gps)
-        location = CoordinatestoLocation(latitude, longitude)
         if latitude is not None and longitude is not None:
+            location = CoordinatestoLocation(latitude, longitude)
             print("\nLocation: ", location)
             print("Coordinates: {:.6f}, {:.6f}".format(latitude, longitude))
             break
