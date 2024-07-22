b5fac83d8f735417271b05ef921f38fd7aa9ca33
Adli Naufal <adlinaufal07@gmail.com>
2024-07-23 02:31:22 +0800

    Testing for syafi

--- Integration/main_1.py
+++ Integration/main_1.py
@@ -100,29 +100,26 @@ def face_reg_runtime():
     
 def lcd_display(name_idx):
     global stop_threads
-    while not stop_threads:
-        if name_idx:
+    if name_idx:
 
-            SPI_DEVICE = "/dev/spidev1.0"
+        SPI_DEVICE = "/dev/spidev1.0"
 
-            disp = LCD2inch4_lib.LCD_2inch4(11, 40, SPI_DEVICE)
-            disp.lcd_init_2inch4()
+        disp = LCD2inch4_lib.LCD_2inch4(11, 40, SPI_DEVICE)
+        disp.lcd_init_2inch4()
 
-            # Retrieve the image
-            image_path = os.path.join("images", f"{name_idx}.jpg")
+        # Retrieve the image
+        image_path = os.path.join("images", f"{name_idx}.jpg")
 
-            # Display the obtained image
-            image = Image.open(image_path)
-            image = image.resize((320, 240))
-            disp.lcd_ShowImage(image, 0, 0)
-            time.sleep(1.5)
+        # Display the obtained image
+        image = Image.open(image_path)
+        image = image.resize((320, 240))
+        disp.lcd_ShowImage(image, 0, 0)
+        time.sleep(1.5)
 
-            # Clear the display
-            disp.lcd_init_2inch4()
-            disp.lcd_clear(BLACK)
-            time.sleep(1.5)
-        else:
-            continue
+        # Clear the display
+        disp.lcd_init_2inch4()
+        disp.lcd_clear(BLACK)
+        time.sleep(1.5)
         
 
 def fetching_encoding(current_directory,images_directory,JSON_FILENAME):
