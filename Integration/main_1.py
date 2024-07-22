1fa9b628ed6f5dedeb05bc78a5e68a3fca356a51
Adli Naufal <adlinaufal07@gmail.com>
2024-07-23 02:04:14 +0800

    Testing for syafi

--- Integration/main_1.py
+++ Integration/main_1.py
@@ -105,7 +105,7 @@ def lcd_display(name_idx):
     while not stop_threads:
         
         if name_idx:
-            lock.acquire()
+            #lock.acquire()
             SPI_DEVICE = "/dev/spidev1.0"
 
             disp = LCD2inch4_lib.LCD_2inch4(11, 40, SPI_DEVICE)
@@ -123,7 +123,7 @@ def lcd_display(name_idx):
             # Clear the display
             disp.lcd_init_2inch4()
             disp.lcd_clear(BLACK)
-            lock.release()
+            #lock.release()
         else:
             continue
         
