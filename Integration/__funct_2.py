8796a2d6c60e4b95334bc4053939322ba01b9502
Adli Naufal <adlinaufal07@gmail.com>
2024-07-23 12:29:49 +0800

    Testing

--- Integration/__funct_2.py
+++ Integration/__funct_2.py
@@ -2,6 +2,7 @@ import cv2
 import face_recognition
 import pickle as pkl
 import os
+import os
 import time
 import re
 import requests
@@ -9,14 +10,15 @@ from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from Gtrans_enc import img_encoder  # Assuming transmission_encodegen.py contains img_encoder function
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont  # PIL library for creating placeholder images
 import traceback
+from PIL import Image
 import threading
 import sys
 import serial
 from gps_utils import GetGPSData, uart_port, CoordinatestoLocation
 
-def img_encoder():  # [DONE]
+def img_encoder(): 
     # Importing images
     absolute_path = os.path.dirname(__file__)
     relative_path = "images"
@@ -28,17 +30,26 @@ def img_encoder():  # [DONE]
 
     for path in PathList:
         img = cv2.imread(os.path.join(folderPath, path))
-        imgList.append(img)
-        individual_ID.append(path[:-4])
-        # print(individual_ID)
+        if img is not None:
+            # Resize image to a consistent aspect ratio, e.g., 640x480
+            target_width = 640
+            target_height = 480
+            img = resize_image(img, target_width, target_height)
+            imgList.append(img)
+            individual_ID.append(path[:-4])
+        else:
+            print(f"Warning: Unable to read image '{path}'.")
 
     # Creating the encodings
     def findEncodings(imagesList):
         encoded_list = []
         for img in imagesList:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            encode = face_recognition.face_encodings(img)[0]
-            encoded_list.append(encode)
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            encodings = face_recognition.face_encodings(img_rgb)
+            if encodings:
+                encoded_list.append(encodings[0])
+            else:
+                print("No faces found in image.")
         return encoded_list
 
     print("Encoding Started")
@@ -46,17 +57,40 @@ def img_encoder():  # [DONE]
     encodeListKnown_withID = [encodeListKnown, individual_ID]
     print("Encoding Complete")
 
-    file = open(os.path.join(absolute_path, "EncodedFile.p"), "wb")
-    pkl.dump(encodeListKnown_withID, file)
-    file.close()
+    with open(os.path.join(absolute_path, "EncodedFile.p"), "wb") as file:
+        pkl.dump(encodeListKnown_withID, file)
     print("File Saved")
 
-def create_img_file():  # [DONE]
+def resize_image(image, target_width, target_height):
+    h, w = image.shape[:2]
+    aspect_ratio = w / h
+    if aspect_ratio > 1:  # Landscape or square image
+        new_width = target_width
+        new_height = int(target_width / aspect_ratio)
+    else:  # Portrait image
+        new_width = int(target_height * aspect_ratio)
+        new_height = target_height
+    
+    # Ensure new dimensions are within the target size
+    new_width = min(new_width, target_width)
+    new_height = min(new_height, target_height)
+    
+    resized_img = cv2.resize(image, (new_width, new_height))
+    
+    # Create a new image with target size and place the resized image in the center
+    new_image = 255 * np.ones((target_height, target_width, 3), dtype=np.uint8)
+    x_offset = (target_width - new_width) // 2
+    y_offset = (target_height - new_height) // 2
+    new_image[y_offset:y_offset + new_height, x_offset:x_offset + new_width] = resized_img
+
+    return new_image
+
+def create_img_file(): #[DONE]
     current_directory = os.path.dirname(os.path.abspath(__file__))
     images_directory = os.path.join(current_directory, 'images')
     if not os.path.exists(images_directory):
         os.makedirs(images_directory)
-    return os.path.dirname(os.path.abspath(__file__)), os.path.join(current_directory, 'images')
+        return os.path.dirname(os.path.abspath(__file__)),os.path.join(current_directory, 'images')
 
 def extract_file_id(url):
     pattern = r'id=([a-zA-Z0-9-_]+)'
@@ -103,29 +137,32 @@ def create_placeholder_image(filepath):
 def sanitize_filename(name):
     return re.sub(r'[/\\: ]', '_', name)
 
-def download_img(current_directory, images_directory, JSON_FILENAME):
+def download_img(current_directory,images_directory,JSON_FILENAME):
     try:
         SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1bqCo5PmQVNV7ix_kQarfSCTYC72P1c-qvrmTcu_Xb4E/edit?usp=sharing'  # Your Google Spreadsheet URL
         SHEET_NAME = 'Form Responses 1'  # Name of the specific sheet within your Google Spreadsheet
 
-        SERVICE_ACCOUNT_FILE = os.path.join(current_directory, JSON_FILENAME + '.json')
+        #current_directory = os.path.dirname(os.path.abspath(__file__))#[done1]
+        SERVICE_ACCOUNT_FILE = os.path.join(current_directory, JSON_FILENAME+'.json')
 
         scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
         creds = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNT_FILE, scope)
         client = gspread.authorize(creds)
 
-        if not os.path.exists(images_directory):
-            os.makedirs(images_directory)
+        #images_directory = os.path.join(current_directory, 'images')#[done1]
+        if not os.path.exists(images_directory):#[done1]
+            os.makedirs(images_directory)#[done1]
 
         spreadsheet = client.open_by_url(SPREADSHEET_URL)
         worksheet = spreadsheet.worksheet(SHEET_NAME)
         data = worksheet.get_all_records()
         row_count = len(data)
-        print("Available data on Data Base ", row_count)
+        print("Available data on Data Base ",row_count)
 
         current_file_names = set()
         new_images_downloaded = False
 
+        # Get the index of the "Location_coordinate" column, create it if it doesn't exist
         headers = worksheet.row_values(1)
         for index, item in enumerate(data, start=2):
             if 'Name' in item and 'Guest_Profile_Picture' in item and 'Timestamp' in item:
@@ -138,33 +175,22 @@ def download_img(current_directory, images_directory, JSON_FILENAME):
                 file_name = f"{sanitized_name}_{sanitized_timestamp}.jpg"
                 file_path = os.path.join(images_directory, file_name)
                 current_file_names.add(file_name)
-
+                
+                # Update the Google Sheet with the sanitized timestamp in 'timestamp_id' column
                 worksheet.update_cell(index, worksheet.find('timestamp_id').col, sanitized_timestamp)
 
                 if not os.path.exists(file_path):
                     download_image_from_drive(image_url, images_directory, sanitized_name, sanitized_timestamp)
                     new_images_downloaded = True
+                    
+                    # Ask for location only when new data is detected
 
                 else:
                     print(f"Data exists: '{file_name}'")
 
-                # Adding padding to make the image 4:3 aspect ratio
                 image = Image.open(file_path)
                 image = image.convert('RGB')
-                orig_width, orig_height = image.size
-
-                target_ratio = 4 / 3
-                current_ratio = orig_width / orig_height
-
-                if current_ratio > target_ratio:
-                    new_width = orig_width
-                    new_height = int(orig_width / target_ratio)
-                else:
-                    new_width = int(orig_height * target_ratio)
-                    new_height = orig_height
-
-                new_image = Image.new('RGB', (new_width, new_height), (255, 255, 255))
-                new_image.paste(image, ((new_width - orig_width) // 2, (new_height - orig_height) // 2))
+                new_image = image.resize((960,720))
                 new_image.save(file_path)
 
             else:
@@ -179,13 +205,12 @@ def download_img(current_directory, images_directory, JSON_FILENAME):
     except Exception as e:
         print("An error occurred:", e)
 
-
-def remove_deleted_images(current_directory, images_directory, JSON_FILENAME):
-    SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1bqCo5PmQVNV7ix_kQarfSCTYC72P1c-qvrmTcu_Xb4E/edit?usp=sharing'
-    SHEET_NAME = 'Form Responses 1'
+def remove_deleted_images(current_directory,images_directory,JSON_FILENAME):
+    SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1bqCo5PmQVNV7ix_kQarfSCTYC72P1c-qvrmTcu_Xb4E/edit?usp=sharing'  # Your Google Spreadsheet URL
+    SHEET_NAME = 'Form Responses 1'  # Name of the specific sheet within your Google Spreadsheet
 
     JSON_FILENAME = "sidp-facialrecognition-21f79db4b512"
-    SERVICE_ACCOUNT_FILE = os.path.join(current_directory, JSON_FILENAME + '.json')
+    SERVICE_ACCOUNT_FILE = os.path.join(current_directory, JSON_FILENAME+'.json')
 
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name(SERVICE_ACCOUNT_FILE, scope)
@@ -214,7 +239,7 @@ def remove_deleted_images(current_directory, images_directory, JSON_FILENAME):
 
     for image_file in all_files:
         dir_file_names.add(image_file)
-
+        
     deleted_files = dir_file_names - database_file_names
     for file_name in deleted_files:
         file_path = os.path.join(images_directory, file_name)
